065a9f045b55b16f87aeb87866c6b7e5b7cfb97b
Kasra Nadri <kasra.n2018@gmail.com>
2025-02-20 13:38:18 +0330

    completed the multiple linear regression model

--- Linear_Regression/Multiple/main.py
+++ Linear_Regression/Multiple/main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
@@ -12,11 +13,18 @@ y = dataset.iloc[:, -1].values
 #========== ENCODING THE CATEGORICAL COLUMN ==========#
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder= 'passthrough')
 X = np.array(ct.fit_transform(X))
+# print(X)
 
 #===== SPLITTING THE DATASET INTO A TRAIN SET AND TEST SET ======#
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 
+#===== TRAINING THE MULTIPLE LINEAR REGRESSION ON THE TRAINING SET =====#
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
 
-
+#===== PREDICTING THE TEST SET RESULTS =====#
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), axis=1))
 
