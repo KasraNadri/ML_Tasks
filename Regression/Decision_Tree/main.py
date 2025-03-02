d13a595cca9eea2c0d58ae0360e2c476ea22a785
KasraNadri <kasra.n2018@gmail.com>
2025-03-02 15:02:49 +0330

    added decision tree regression (without the visualization)

---
+++ Regression/Decision_Tree/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.tree import DecisionTreeRegressor
+
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:- 1].values
+y = dataset.iloc[:, -1].values
+
+
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X, y)
+
+print(regressor.predict([[6,5]]))
