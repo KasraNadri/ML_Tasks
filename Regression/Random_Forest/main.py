594ecfa9920fa743ce9527e5c49941b714705dee
KasraNadri <kasra.n2018@gmail.com>
2025-03-03 09:38:57 +0330

    completed random_forest_regression and done with regression section

--- Regression/Random_Forest/main.py
+++ Regression/Random_Forest/main.py
@@ -2,21 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:- 1].values
 y = dataset.iloc[:, -1].values
 
 
-regressor = DecisionTreeRegressor(random_state=0)
+regressor = RandomForestRegressor(n_estimators=10, random_state=0)
 regressor.fit(X, y)
 
-X_grid = np.arange(min(X), max(X), 0.1)
-X_grid = X_grid.reshape(len(X_grid), 1)
+
+
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.title('Truth or Bluff (Random Forest Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
